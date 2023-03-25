ce06b1d96a5496a505e6b7c2e13ae9e5b1a6e0a1
Kshah002 <100991997+Kshah002@users.noreply.github.com>
2023-03-25 22:07:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ import numpy as np
 
 cred = credentials.Certificate("ServiceAccountKey.json")
 firebase_admin.initialize_app(cred, {
-    'databaseURL': "https://facerecognitionrealtime-267f2-default-rtdb.firebaseio.com/",
-    'storageBucket': "facerecognitionrealtime-267f2.appspot.com"
+    'databaseURL': "firebase_url",
+    'storageBucket': "storage_url"
 })
 bucket = storage.bucket()
 
